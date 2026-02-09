24da8c9eec5c50dfd6c1e36fda75c443570cccef
anti-paper <nakaji361ll@gmail.com>
2026-02-10 08:08:37 +0900

    アセンブリでHello World出力するよう修正

--- src/assembly/sample.s
+++ src/assembly/sample.s
@@ -1,6 +1,18 @@
 .global main
 
-main:
-    mov x0, #5
+.section .data
+msg:
+    .ascii "Hello World!\n"
+len = . - msg
 
-    ret
+.section .text
+main:
+    mov x0, #1
+    ldr x1, =msg
+    mov x2, #len
+    mov x8, #64
+    svc #0
+
+    mov x0, #0
+    mov x8, #93
+    svc #0
